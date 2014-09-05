38149c946083d1a384cae72093a9318152e4623f
Marko Locher <marko@locher.at>
2014-09-05 12:03:30 +0200

    Use the title provided via metadata for category and tag pages
    * Don't capitalize the title by default, but respect the way its provided
    * Fall back to sensible defaults if none is available
      * Category Page: capitalized category name
      * Tag page: tag name prepended by a hash sign

--- _plugins/tag_pages.rb
+++ _plugins/tag_pages.rb
@@ -10,7 +10,6 @@ module Jekyll
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tags.html')
       self.data['tag'] = tag
-      self.data['title'] = "#{tag}"
     end
   end
 
